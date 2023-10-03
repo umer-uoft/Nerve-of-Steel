290ac82e21cbb0655c1bfdcb3649abb4692a3db2
Umer Memon <umer@Umers-MacBook-Pro.local>
2023-10-03 18:12:44 -0400

    Added a list of participating players, who are randomly chosen to win the game to nos.py

--- nos.py
+++ nos.py
@@ -10,14 +10,21 @@ timer.py uses the time library to help keep track of time
 
 
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
-from PIL import Image # the pillow library makes it easy to display images 
-import random
+import random # The random library has a feature that randomly chooses numbers from a range, or a name from the list of players participating in the game.
 
 #program displays "players stand!"
 print("Players, please stand!")
 
+#timer is set randomly to anything in between 5-25 seconds.
 timer_random = random.choice(range(5,25))
 time.sleep(timer_random)
 
+#once the time runs out, the text below is shown.
 print('Times up! Last to sit down wins.')
+
+#list of participating players is setup, and a random player is chosen to be the winner.
+playerlist = ["Emma", "Fatima", "John", "Bob", "Umer"]
+winner = random.choice(playerlist)
+print('The winner is', winner + '!')
+
 
