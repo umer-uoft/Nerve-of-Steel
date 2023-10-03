1af85c09f9fbecd4e1acbf22ff7956d82c8f8f62
Umer Memon <umer@Umers-MacBook-Pro.local>
2023-10-03 18:49:42 -0400

    removed irrelevant comments to clean up the code.

--- nos.py
+++ nos.py
@@ -1,15 +1,4 @@
-"""
-This program is used as an example for MGTC28.
-timer.py is a simple Python script that will allow user to set timer duration.
-Upon timer expiry, user will see the time up meme and sound notification.
-timer.py uses the time library to help keep track of time
-"""
-
-
-# This program is timer that counts down
-
-
-import time # The time library has a sleep function that will pause the script for a specifized amount of time
+import time # The time library has a sleep function that will pause the script for a specified amount of time
 import random # The random library has a feature that randomly chooses numbers from a range, or a name from the list of players participating in the game.
 
 #program displays "players stand!"
